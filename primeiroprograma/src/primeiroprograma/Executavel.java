fa44526f680036e3aece67f3726200c8bcb2a673
aluno <aluno@192.168.0.78>
2024-11-27 16:38:10 -0300

    Segundo commit

--- primeiroprograma/src/primeiroprograma/Executavel.java
+++ primeiroprograma/src/primeiroprograma/Executavel.java
@@ -5,5 +5,16 @@ public class Executavel {
 	public static void main(String[] args) {
 		System.out.println("Olá Mundo");
 		System.out.println("Meu nome é Mateus");
+		
+		int inteiro;
+		double numeroDecimal;
+		String texto;
+		inteiro = 5;
+		numeroDecimal = 5.65;
+		texto = "Mateus";
+		System.out.println("Nome:" + inteiro);
+		System.out.println("Numero Decimal"+ numeroDecimal);
+		
+		
 	}
 }
